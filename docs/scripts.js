8e028817ee1ca763fb15fa08f13f18a7c1960025
Kamalaysayan <mmckamalaysayanclub@gmail.com>
2025-02-20 00:32:05 +0800

    move files to docs directory

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,12 +1,12 @@
 function navigateToEditor() {
     const password = prompt('Enter password:');
     if (password === 'Kamalaysayan@2025') {
-        window.location.href = '/editor.html'; // Correct file path
+        window.location.href = 'editor.html'; // Remove leading slash
     } else {
         alert('Incorrect password');
     }
 }
 
 function navigateToCustomer() {
-    window.location.href = '/customer.html'; // Correct file path
+    window.location.href = 'customer.html'; // Remove leading slash
 }
